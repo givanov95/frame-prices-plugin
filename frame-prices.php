c1210912d29eb29970b46e2ca39c0286ddef55d4
givanov95 <joroz95@gmail.com>
2021-06-29 14:48:41 +0300

    add new version number

--- frame-prices.php
+++ frame-prices.php
@@ -4,7 +4,7 @@
  * Plugin Name: Цени на каси
  * Plugin URI: 
  * Description: 
- * Version: 0.3.95
+ * Version: 0.3.96
  * Author: Martin Mladenov, Georgi Ivanov
  * Author URI: http://yourwebsiteurl.com/
  **/
